551f5339dbbb90d379c5f55563054061982de833
VincentVillaflores <vdvillaflores@icloud.com>
2023-07-26 12:39:43 +1000

    added initial read of file and appending to thewriter

--- a1-startup/copier_files/reader.cpp
+++ a1-startup/copier_files/reader.cpp
@@ -5,6 +5,16 @@
 #include "reader.h"
 #include <functional>
 
-reader::reader(const std::string& name, writer& mywriter) {}
+reader::reader(const std::string& name, writer& mywriter) {
+    std::ifstream in(name);
+}
 
-void reader::run() {}
+void reader::run() {
+    if (in.is_open()) {
+        std::string line;
+        while (std::getline(in, line)){
+            thewriter.append(line);
+        }
+        in.close();
+    }
+}
