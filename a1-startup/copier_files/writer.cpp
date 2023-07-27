2d94017e29274fab1252fa0615e88d0487a95ebc
VincentVillaflores <vdvillaflores@icloud.com>
2023-07-28 08:51:03 +1000

    implemented run() for writer

--- a1-startup/copier_files/writer.cpp
+++ a1-startup/copier_files/writer.cpp
@@ -12,7 +12,15 @@ writer::writer(const std::string& name) {
     std::ofstream out(name);
 }
 
-void writer::run() {}
+void writer::run() {
+    if (out.is_open()){
+        for (std::string line : queue){
+            out << line << std::endl;
+        }
+    }
+
+    out.close();
+}
 
 void writer::append(const std::string& line) {
     queue.push_back(line);
