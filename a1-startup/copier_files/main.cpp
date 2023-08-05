7b7c584e261ea8205ced41dde8aae2d22e6ef977
VincentVillaflores <vdvillaflores@icloud.com>
2023-08-05 11:10:41 +1000

    added check for input count

--- a1-startup/copier_files/main.cpp
+++ a1-startup/copier_files/main.cpp
@@ -8,10 +8,15 @@
 /* global variables if needed go here */
 int main(int argc, char** argv) {
     /* check command line arguments */
-    writer write(argv[2]);
-    reader read(argv[1], write);
-    read.run();
-    write.run();
+    if (argc < 3) {
+        std::cout<<"Please pass an input and output file!"<<std::endl;
+    }
+    else {
+        writer write(argv[2]);
+        reader read(argv[1], write);
+        read.run();
+        write.run();
+    }
     
     /* load the file and copy it to the destination */
     return EXIT_SUCCESS;
