0e445fedc7157ade7e7902e25bdf5139234ececc
VincentVillaflores <vdvillaflores@icloud.com>
2023-08-17 09:13:21 +1000

    removed duration vector

--- a1-startup/TimerStruct.h
+++ a1-startup/TimerStruct.h
@@ -7,10 +7,8 @@
 struct TimerStruct {
     std::vector<clock_t> readLineStart;
     std::vector<clock_t> readLineEnd;
-    std::vector<clock_t> readLineDuration;
     std::vector<clock_t> writeLineStart;
     std::vector<clock_t> writeLineEnd;
-    std::vector<clock_t> writeLineDuration;
 };
 
 #endif // TIMERSTRUCT_H
